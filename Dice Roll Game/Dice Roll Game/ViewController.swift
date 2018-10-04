a07b1e4a630ab77403f9a5237d03fadc803cd111
DeavyJ <joelvangente--4@hotmail.com>
2018-10-04 12:50:41 +0100

    added button functionality

--- Dice Roll Game/Dice Roll Game/ViewController.swift
+++ Dice Roll Game/Dice Roll Game/ViewController.swift
@@ -9,12 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var lblDiceresum: UILabel!
+    
+    @IBOutlet weak var imgDice1: UIImageView!
+    
+    @IBOutlet weak var imgDice2: UIImageView!
+    
+    var sum = 0
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func btnRoll(_ sender: Any) {
+        lblDiceresum.text = String(sum)
+        let dice1roll = arc4random_uniform(6) + 1
+        let dice2roll = arc4random_uniform(6) + 1
+        
+        imgDice1.image = UIImage(named: "Dice\(dice1roll)")
+        imgDice2.image = UIImage(named: "Dice\(dice2roll)")
+        
+    }
+    
+    
+    
 }
 
