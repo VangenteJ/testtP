4a70399f176e915fa5c3a3563b2bc8664435c256
DeavyJ <joelvangente--4@hotmail.com>
2018-10-04 21:44:18 +0100

    changes

---
+++ TrueConvertorX/TrueConvertorX/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TrueConvertorX
+//
+//  Created by Joel Vangente on 04/10/2018.
+//  Copyright © 2018 Joel Vangente. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ TrueConvertorX/TrueConvertorX/ViewControllerSecond.swift
@@ -0,0 +1,20 @@
+//
+//  ViewControllerSecond.swift
+//  TrueConvertorX
+//
+//  Created by Joel Vangente on 04/10/2018.
+//  Copyright © 2018 Joel Vangente. All rights reserved.
+//
+
+import UIKit
+
+class ViewControllerSecond: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+ 
+
+}
