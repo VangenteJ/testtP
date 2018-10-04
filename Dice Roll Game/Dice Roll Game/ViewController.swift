ae3844688f83957220551ec37dd65932b9e8bfed
DeavyJ <joelvangente--4@hotmail.com>
2018-10-04 09:57:43 +0100

    Dice roll game

---
+++ Dice Roll Game/Dice Roll Game/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dice Roll Game
+//
+//  Created by Joel Vangente on 04/10/2018.
+//  Copyright © 2018 Joel Vangente. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
