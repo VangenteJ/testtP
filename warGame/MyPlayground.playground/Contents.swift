c0d13b988d26656fd75a127d1ca8330136d89e17
DeavyJ <joelvangente--4@hotmail.com>
2018-10-04 09:43:58 +0100

    swift war games

---
+++ warGame/MyPlayground.playground/Contents.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+var str = "Hello, playground"
+
+var a = ["Cat", "Dog", "Plung", "Turtle"]
+
+for item in a
+{
+    if item != "Cat" && item != "Dog" && item != "Turtle"
+    {
+      print(item)
+    }
+}
+
--- warGame/warGame/warGame/ViewController.swift
+++ warGame/warGame/warGame/ViewController.swift
@@ -9,11 +9,43 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var leftImage: UIImageView!
+    
+    @IBOutlet weak var rightImage: UIImageView!
+    
+    @IBOutlet weak var lblPlayerScore: UILabel!
+    
+    @IBOutlet weak var lblCPUScore: UILabel!
+    
+    var playerCount = 0
+    var cpuCount = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    var c = 0
+    @IBAction func btnDealTapper(_ sender: Any)
+    {
+        let leftRandomNumber = arc4random_uniform(13) + 2
+        let rightRandomNumber = arc4random_uniform(13) + 2
+        
+        leftImage.image = UIImage(named: "card\(leftRandomNumber)")
+        rightImage.image = UIImage(named: "card\(rightRandomNumber)")
+        
+        if leftRandomNumber > rightRandomNumber
+        {
+            playerCount += 1
+            lblPlayerScore.text = String(playerCount)
+        }else if leftRandomNumber < rightRandomNumber
+        {
+            cpuCount += 1
+            lblCPUScore.text = String(cpuCount)
+        }
+    }
+    
 
 
 }
