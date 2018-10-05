6e1b73f49c61fb862821922476df85f77f4f5f8f
DeavyJ <joelvangente--4@hotmail.com>
2018-10-05 21:29:12 +0100

    progress

--- TrueConvertorX/TrueConvertorX/ViewController.swift
+++ TrueConvertorX/TrueConvertorX/ViewController.swift
@@ -6,15 +6,69 @@
 //  Copyright © 2018 Joel Vangente. All rights reserved.
 //
 
+var firstBackground = "white"
+
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var viewMain: UIView!
+    
+    @IBOutlet weak var lblTopText: UILabel!
+    
+    @IBOutlet weak var lblMiddletext: UILabel!
+    
+    @IBOutlet weak var btnButton: UIButton!
+    
+    @IBOutlet weak var lblBottomText: UILabel!
+    
+    
+    @IBAction func btnNext(_ sender: Any) {
+    }
+    
+    @IBAction func btnChangeWhite(_ sender: Any) {
+        viewMain.backgroundColor = UIColor.white
+        firstBackground = "white"
+        colorChangeDefault()
+    }
+    
+    @IBAction func btnChangeLBlue(_ sender: Any) {
+        viewMain.backgroundColor = UIColor.cyan
+        firstBackground = "cyan"
+        colorChangeDefault()
+    }
+    
+    @IBAction func btnChangeDBlue(_ sender: Any) {
+        viewMain.backgroundColor = UIColor.blue
+        firstBackground = "blue"
+        colorChange()
+    }
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
 
+    func colorChange(){
+        lblTopText.textColor = UIColor.cyan
+        lblMiddletext.textColor = UIColor.cyan
+        btnButton.setTitleColor(.cyan, for:.normal)
+        lblBottomText.textColor = UIColor.cyan
+    }
+    
+    func colorChangeDefault(){
+        lblTopText.textColor = UIColor.black
+        lblMiddletext.textColor = UIColor.black
+        btnButton.setTitleColor(.blue, for:.normal)
+        lblBottomText.textColor = UIColor.black
+    }
+    
+    
 }
 
--- TrueConvertorX/TrueConvertorX/ViewControllerSecond.swift
+++ TrueConvertorX/TrueConvertorX/ViewControllerSecond.swift
@@ -9,7 +9,47 @@
 import UIKit
 
 class ViewControllerSecond: UIViewController {
-
+    
+    @IBOutlet var viewSecond: UIView!
+    
+    @IBOutlet weak var lblHeadText: UILabel!
+    
+    @IBOutlet weak var txtUserInput: UITextField!
+    
+    @IBOutlet weak var lblOutput: UILabel!
+    
+    
+    
+    
+    
+    @IBAction func btnCMtoIN(_ sender: Any) {
+    }
+    
+    @IBAction func btnCtoF(_ sender: Any) {
+    }
+    
+    @IBAction func btnSTtoKG(_ sender: Any) {
+    }
+    
+    @IBAction func btnINtoCM(_ sender: Any) {
+    }
+    
+    @IBAction func btnFtoC(_ sender: Any) {
+    }
+    
+    @IBAction func KGtoST(_ sender: Any) {
+    }
+    
+    @IBAction func btnChangeWhite(_ sender: Any) {
+    }
+    
+    @IBAction func btnChangeBlue(_ sender: Any) {
+    }
+    
+    @IBAction func btnChangeLightBlue(_ sender: Any) {
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
