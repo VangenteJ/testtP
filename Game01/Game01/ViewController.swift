19a8c3b0176c149994a4acdeae5565f0871dfa78
DeavyJ <joelvangente--4@hotmail.com>
2018-10-07 08:35:27 +0100

    Simple layout game

---
+++ Game01/Game01/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Game01
+//
+//  Created by Joel Vangente on 07/10/2018.
+//  Copyright © 2018 Joel Vangente. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Game01/Game01Tests/Game01Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Game01Tests.swift
+//  Game01Tests
+//
+//  Created by Joel Vangente on 07/10/2018.
+//  Copyright © 2018 Joel Vangente. All rights reserved.
+//
+
+import XCTest
+@testable import Game01
+
+class Game01Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
