98c0177381bf82d5b19d33381ebecfa8cc27c0ac
DeavyJ <joelvangente--4@hotmail.com>
2018-10-18 13:14:57 +0100

    date and time

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,14 +1,15 @@
 import UIKit
 
-class Joel{
-    
-    var make:String?
-    var type:String?
-    
-    init(make:String, type:String){
-        self.make = "ford"
-        self.type = "4X4"
-    }
-    var x = 10
-    //print("make")
-}
+var str = "Hello, playground"
+
+let date = Date()
+let calendar = Calendar.current
+
+let hour = calendar.component(.hour, from: date)
+let minutes = calendar.component(.minute, from: date)
+let seconds = calendar.component(.second, from: date)
+let day = calendar.component(.day, from: date)
+let month = calendar.component(.month, from: date)
+print ("\(hour):\(minutes):\(seconds)")
+print ("\(day)/\(month)")
+
