a3cd9fe7678792e025a018e1fc5cd67de6fc4cdd
DeavyJ <joelvangente--4@hotmail.com>
2018-10-24 10:49:49 +0100

    steps count

---
+++ ApplePaytest/ApplePaytest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ApplePaytest
+//
+//  Created by Joel Vangente on 24/10/2018.
+//  Copyright © 2018 Joel Vangente. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
