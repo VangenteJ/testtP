1dc841cb2c9ab329492f87b4586767b1fae4790f
DeavyJ <joelvangente--4@hotmail.com>
2018-10-25 20:24:41 +0100

    standard payment

--- ApplePaytest/ApplePaytest/AppDelegate.swift
+++ ApplePaytest/ApplePaytest/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Stripe
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,6 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        STPPaymentConfiguration.shared().publishableKey = "pk_test_iU8azdSMJFtDUlYI9l0BOS7Q"
         // Override point for customization after application launch.
         return true
     }
--- ApplePaytest/ApplePaytest/ViewController.swift
+++ ApplePaytest/ApplePaytest/ViewController.swift
@@ -7,12 +7,41 @@
 //
 
 import UIKit
+import Stripe
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, STPAddCardViewControllerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    @IBAction func btnpay(_ sender: Any) {
+        // Setup add card view controller
+        let addCardViewController = STPAddCardViewController()
+        addCardViewController.delegate = self
+        
+        // Present add card view controller
+        let navigationController = UINavigationController(rootViewController: addCardViewController)
+        present(navigationController, animated: true)
+    }
+    
+    func addCardViewControllerDidCancel(_ addCardViewController: STPAddCardViewController) {
+        // Dismiss add card view controller
+        dismiss(animated: true)
+    }
+    
+    func addCardViewController(_ addCardViewController: STPAddCardViewController, didCreateToken token: STPToken, completion: @escaping STPErrorBlock) {
+        dismiss(animated: true)
+        
+        print("Printing Strip response:\(token.allResponseFields)\n\n")
+        print("Printing Strip Token:\(token.tokenId)")
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
     }
 
 
